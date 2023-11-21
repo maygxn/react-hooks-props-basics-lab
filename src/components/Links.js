8f92d6075cb0618db8ea29f2f52dfcdfc614be00
Meagan <MeaganTruong1@yahoo.com>
2023-11-20 20:30:44 -0600

    finished

---
+++ src/components/Links.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Links(user) {
+    return (
+        <div>
+            <h3>Links</h3>
+            <a href={user.github}>{user.github}</a>
+            <a href={user.linkedin}>{user.linkedin}</a>
+        </div>
+    )
+}
+
+export default Links;
